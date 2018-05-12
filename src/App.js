cf1f65773ba1794553f795ae4faca040e9a1eb62
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-05-12 14:55:29 -0500

    Background with css

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-title">
-          Clocky Countdown Challenge
-        </div>
-        <div>
-          <div>Days</div>
-          <div>Hours</div>
-          <div>Minutes</div>
-          <div>Seconds</div>
-        </div>
-        <div>
-          <input placeholder="new date"/>
-          <button>Submit</button>
+      <div className="All">
+        <div className="App">
+          <div className="App-title">
+            Clocky Countdown Challenge
+          </div>
+          <div>
+            <div>Days</div>
+            <div>Hours</div>
+            <div>Minutes</div>
+            <div>Seconds</div>
+          </div>
+          <div>
+            <input placeholder="new date"/>
+            <button>Submit</button>
+          </div>
         </div>
       </div>
     );
